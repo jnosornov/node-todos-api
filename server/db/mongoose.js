8b50a50722094995ad9329f3272833df7cfdabc5
Johnathan Osorno <jnosornov@gmail.com>
2018-03-20 10:47:11 -0500

    Add MongoDB URI environment variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-// > connect mongoose to our database
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
